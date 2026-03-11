e898ead9085b40f073b2ffd0a418ac6c6828503d
chenzixuan99 <835846197@qq.com>
2026-03-11 16:37:56 +0800

    Fix: use relative base path to resolve asset loading issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/adaclear-grasp.github.io/'
+  base: './'
 });
